ee01ad00ea873d4aa562298077aae11dd448e2b1
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-04-14 10:43:06 -0300

    Criando a main_window

---
+++ ui/main_window.py
@@ -0,0 +1,16 @@
+import customtkinter as ctk
+import tkinter.messagebox as mb
+from  utils.layout import *
+from components.DefaultButton import DefaultButton
+from components.LabelEntry import LabelEntry
+from config.theme import COLOR_BACKGROUND
+
+class MainScreen(ctk.CTk):
+
+    def __init__(self):
+        super().__init__(fg_color=COLOR_BACKGROUND)
+
+        self.title("Login")
+        self.geometry("500x400")
+        
+        self.after(10, lambda: center_window(self))
